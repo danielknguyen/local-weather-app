f387419614b1ae6fcdde45e59ad641bdae4e8375
Daniel Nguyen <danielnguyen93@hotmail.com>
2017-04-23 14:36:56 -0700

    crossDomain:true

--- script.js
+++ script.js
@@ -8,36 +8,42 @@ $(document).ready(function(){
 		weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=8478db85cf8851584a9cfd828d602f11';
 
 		// Asynchronous retrieving of weather data
-		$.getJSON(weatherUrl, function(data){
+		$.ajax({
+			url:weatherUrl,
+			method:'get',
+			crossDomain:true,
+			success: function(data) {
+				weatherLocation = data.sys.country;
+				weatherDescription = data.weather[0].description;
+				weatherKelvin = data.main.temp;
+				weatherFahrenheit = Math.floor(9/5 * weatherKelvin - 459.67);
+				weatherCelsius = Math.floor((weatherFahrenheit - 32) * 5/9);
+				weatherImage = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
 
-			weatherLocation = data.sys.country;
-			weatherDescription = data.weather[0].description;
-			weatherKelvin = data.main.temp;
-			weatherFahrenheit = Math.floor(9/5 * weatherKelvin - 459.67);
-			weatherCelsius = Math.floor((weatherFahrenheit - 32) * 5/9);
-			weatherImage = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
+				console.log(data);
 
-			console.log(data);
-
-			//add user location
-			$('.weather-location')
-				.text(weatherLocation + ',')
-				.css('font-size', '2.5em')
-				.css('color', '#FFFFFF');
-			//add description of weather
-			$('.weather-description')
-				.text(weatherDescription)
-				.css('font-size', '2.5em')
-				.css('color', '#FFFFFF');
-			//add temperature in fahrenheit
-			$('.weather-temperature')
-				.text(weatherFahrenheit + ' F')
-				.css('font-size', '2.5em')
-				.css('color', '#FFFFFF');
-			//add weather icon according to the specific weather
-			$('.weather-image')
-				.prepend("<img src='" + weatherImage + "'" + "/>")
-		 	});
+				//add user location
+				$('.weather-location')
+					.text(weatherLocation + ',')
+					.css('font-size', '2.5em')
+					.css('color', '#FFFFFF');
+				//add description of weather
+				$('.weather-description')
+					.text(weatherDescription)
+					.css('font-size', '2.5em')
+					.css('color', '#FFFFFF');
+				//add temperature in fahrenheit
+				$('.weather-temperature')
+					.text(weatherFahrenheit + ' F')
+					.css('font-size', '2.5em')
+					.css('color', '#FFFFFF');
+				//add weather icon according to the specific weather
+				$('.weather-image')
+					.prepend("<img src='" + weatherImage + "'" + "/>")
+			}
+			
+		});
+	
 	//mouse click temperature to change to celsius
 	// $('.weather-temperature').on('click', function(){
 	// 	$('.weather-temperature')
