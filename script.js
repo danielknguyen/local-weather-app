2c63e3f95d990353a7019ccc963b3ef5ea2e2f56
Daniel Nguyen <danielnguyen93@hotmail.com>
2017-04-23 20:55:24 -0700

    added icons

--- script.js
+++ script.js
@@ -44,28 +44,28 @@ $(document).ready(function(){
 					.hide();
 				//add weather icon according to the specific weather
 				var icons = new Skycons({"color": "orange"});
-				
-				if(weatherIcon.indexOf("partly-cloudy-day") >= 0) {
-					icons.set("weather-icon", Skycons.PARTLY_CLOUDY_DAY);	
-				} else if (weatherIcon.indexOf("clear-day") >= 0) {
-					icons.set("weatherDescription", Skycons.CLEAR_DAY);
-				} else if (weatherIcon.indexOf("clear-night") >= 0) {
-					icons.set("weatherDescription", Skycons.CLEAR_NIGHT);
-				} else if (weatherIcon.indexOf("partly-cloudy-night") >= 0) {
-					icons.set("weatherDescription", Skycons.PARTLY_CLOUDY_NIGHT);
-				} else if (weatherIcon.indexOf("cloudy") >= 0) {
-					icons.set("weatherDescription", Skycons.CLOUDY);
-				} else if (weatherIcon.indexOf("rain") >= 0) {
-					icons.set("weatherDescription", Skycons.RAIN);
-				} else if (weatherIcon.indexOf("sleet") >= 0) {
-					icons.set("weatherDescription", Skycons.SLEET);
-				} else if (weatherIcon.indexOf("snow") >= 0) {
-					icons.set("weatherDescription", Skycons.SNOW);
-				} else if (weatherIcon.indexOf("wind") >= 0) {
-					icons.set("weatherDescription", Skycons.WIND);
-				} else if (weatherIcon.indexOf("fog") >= 0) {
-					icons.set("weatherDescription", Skycons.FOG);
-				}
+
+					if(weatherIcon.indexOf("partly-cloudy-night") >= 0) {
+						icons.add("weather-icon", Skycons.PARTLY_CLOUDY_DAY);
+					} else if (weatherIcon.indexOf("clear-day") >= 0) {
+						icons.add("weather-icon", Skycons.CLEAR_DAY);
+					} else if (weatherIcon.indexOf("clear-night") >= 0) {
+						icons.add("weatherDescription", Skycons.CLEAR_NIGHT);
+					} else if (weatherIcon.indexOf("partly-cloudy-night") >= 0) {
+						icons.add("weatherDescription", Skycons.PARTLY_CLOUDY_NIGHT);
+					} else if (weatherIcon.indexOf("cloudy") >= 0) {
+						icons.add("weatherDescription", Skycons.CLOUDY);
+					} else if (weatherIcon.indexOf("rain") >= 0) {
+						icons.add("weatherDescription", Skycons.RAIN);
+					} else if (weatherIcon.indexOf("sleet") >= 0) {
+						icons.add("weatherDescription", Skycons.SLEET);
+					} else if (weatherIcon.indexOf("snow") >= 0) {
+						icons.add("weatherDescription", Skycons.SNOW);
+					} else if (weatherIcon.indexOf("wind") >= 0) {
+						icons.add("weatherDescription", Skycons.WIND);
+					} else if (weatherIcon.indexOf("fog") >= 0) {
+						icons.add("weatherDescription", Skycons.FOG);
+					}
 				icons.play();
 			}
 			
@@ -80,4 +80,5 @@ $(document).ready(function(){
 		})		
 	});	
 });
+
 	
