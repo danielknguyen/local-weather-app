d97b86a13a202ccd2644c0b26f7f93f523e3f5bf
Daniel Nguyen <danielnguyen93@hotmail.com>
2017-04-23 18:28:09 -0700

    added icons

--- script.js
+++ script.js
@@ -5,26 +5,19 @@ $(document).ready(function(){
 		latitude = position.coords.latitude;
 		longitude = position.coords.longitude;
 
-		weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&APPID=8478db85cf8851584a9cfd828d602f11';
+		weatherUrl = 'https://api.darksky.net/forecast/df35bff65738d6e81b904f50a9198a85/' + latitude + ',' + longitude;
 
 		// Asynchronous retrieving of weather data
 		$.ajax({
 			url:weatherUrl,
-			method:'get',
+			type:'GET',
 			dataType:'jsonp',
-			cors:true,
-			contentType:'application,json',
-			secure:true,
-				headers:{
-					'Access-Control-Allow-Origin': '*'
-			},
 			success: function(data) {
-				weatherLocation = data.sys.country;
-				weatherDescription = data.weather[0].description;
-				weatherKelvin = data.main.temp;
-				weatherFahrenheit = Math.floor(9/5 * weatherKelvin - 459.67);
+				weatherLocation = data.timezone;
+				weatherDescription = data.currently.summary;
+				weatherFahrenheit = Math.floor(data.currently.apparentTemperature);
 				weatherCelsius = Math.floor((weatherFahrenheit - 32) * 5/9);
-				weatherImage = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
+				weatherIcon = data.currently.icon;
 
 				console.log(data);
 
@@ -44,8 +37,30 @@ $(document).ready(function(){
 					.css('font-size', '2.5em')
 					.css('color', '#FFFFFF');
 				//add weather icon according to the specific weather
-				$('.weather-image')
-					.prepend("<img src='" + weatherImage + "'" + "/>")
+				var icons = new Skycons({"color": "orange"});
+				
+				if(weatherIcon.indexOf("partly-cloudy-day") >= 0) {
+					icons.set("weather-icon", Skycons.PARTLY_CLOUDY_DAY);	
+				} else if (weatherIcon.indexOf("clear-day") >= 0) {
+					icons.set("weatherDescription", Skycons.CLEAR_DAY);
+				} else if (weatherIcon.indexOf("clear-night") >= 0) {
+					icons.set("weatherDescription", Skycons.CLEAR_NIGHT);
+				} else if (weatherIcon.indexOf("partly-cloudy-night") >= 0) {
+					icons.set("weatherDescription", Skycons.PARTLY_CLOUDY_NIGHT);
+				} else if (weatherIcon.indexOf("cloudy") >= 0) {
+					icons.set("weatherDescription", Skycons.CLOUDY);
+				} else if (weatherIcon.indexOf("rain") >= 0) {
+					icons.set("weatherDescription", Skycons.RAIN);
+				} else if (weatherIcon.indexOf("sleet") >= 0) {
+					icons.set("weatherDescription", Skycons.SLEET);
+				} else if (weatherIcon.indexOf("snow") >= 0) {
+					icons.set("weatherDescription", Skycons.SNOW);
+				} else if (weatherIcon.indexOf("wind") >= 0) {
+					icons.set("weatherDescription", Skycons.WIND);
+				} else if (weatherIcon.indexOf("fog") >= 0) {
+					icons.set("weatherDescription", Skycons.FOG);
+				}
+				icons.play();
 			}
 			
 		});
@@ -63,5 +78,6 @@ $(document).ready(function(){
 	// 		.text(weatherFahrenheit + ' F')
 	// 		.css('font-size', '2.5em')
 	// 		.css('color', '#FFFFFF');
+			
 	});	
 });
